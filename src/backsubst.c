3fa6b7371662d18e4f70cd19c9c6375239858ce8
Tosia <todospial@gmail.com>
2023-12-19 13:48:16 +0100

    dodano funkcje odpowiadajaca za podstawienie wsteczne

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,37 @@
+#include <stdio.h>
 #include "backsubst.h"
+
+
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
+
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	int r = mat->r;
+	int c = mat->c;
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    	if (r != c) {
+        	fprintf(stderr, "Blad: Macierz nie jest kwadratowa.\n");
+        	return 2;
+    	}
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    	for (int i = r-1; i >= 0; i--){
 
-				return 0;
+        	if (mat->data[i][i] == 0) {
+        	    fprintf(stderr, "Blad: Dzielenie przez 0 (element na diagonali = 0).\n");
+        	    return 1;
+       	 	}
+
+	        double s = 0;
+	        for (int j = i+1; j < r; j++) {
+        	    s += mat->data[i][j] * x->data[j][0];
+       	 	}
+        	x->data[i][0] = (b->data[i][0] - s) / mat->data[i][i];
+	}
+
+	return 0;
 }
 
 
