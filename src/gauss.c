d93c43248913e1317812ee62fc5b883f59e25d60
Tosia <todospial@gmail.com>
2023-12-20 22:11:29 +0100

    rozszerzono metode o wybor el. glownego

--- src/gauss.c
+++ src/gauss.c
@@ -1,5 +1,8 @@
 #include "gauss.h"
 #include <math.h>
+#include <stdio.h>
+
+
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
@@ -7,7 +10,22 @@
 int eliminate(Matrix *mat, Matrix *b) {
     int i, j, k;
 
-    for (i = 0; i < mat->r - 1; i++) {
+    for( int i= 0; k < mat->r - 1; i++) {
+        int max = k;
+
+        for (int w = k + 1; w < n; w++)
+            if (fabs(mat->data[w][i]) > fabs(mat->data[max][i]))
+                max = w;
+
+        if (max != i) {
+            double *tmp = mat->data[i];
+            mat->data[i] = mat->data[max];
+            mat->data[max] = tmp;
+            double bt = b->data[max][0];
+            b->data[max][0] = b->data[i][0];
+            b->data[i][0] = bt;
+        }
+
         for (k = i + 1; k < mat->r; k++) {
             double factor = mat->data[k][i] / mat->data[i][i];
             for (j = i; j < mat->c; j++) {
@@ -15,12 +33,12 @@ int eliminate(Matrix *mat, Matrix *b) {
             }
             b->data[k][0] -= factor * b->data[i][0];
         }
-    }
-    for (i = 0; i < mat->r; i++) {
-        if (fabs(mat->data[i][i]) < 1e-10) {
-            return 1;
+        for (i = 0; i < mat->r; i++) {
+            if (fabs(mat->data[i][i]) < 1e-10) {
+                fprintf(stderr, "Macierz osobliwa, dzielenie przez 0\n");
+                return 1;
+            }
         }
     }
-
     return 0;
 }
